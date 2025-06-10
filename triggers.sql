80456b815b171c823dba409e5fd9510bcce5b705
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-06-10 10:33:08 -0300

    Create triggers.sql
    Criação das Triggers

---
+++ triggers.sql
@@ -0,0 +1,17 @@
+CREATE TRIGGER tr_insere_fazenda
+
+CREATE TRIGGER tr_deleta_fazenda
+
+CREATE TRIGGER tr_atualiza_fazenda
+
+CREATE TRIGGER tr_insere_maquinario
+
+CREATE TRIGGER tr_deleta_maquinario
+
+CREATE TRIGGER tr_atualiza_maquinario
+
+CREATE TRIGGER tr_insere_produtor
+
+CREATE TRIGGER tr_deleta_produtor
+
+CREATE TRIGGER tr_atualiza_produtor
